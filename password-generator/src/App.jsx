a50d6e4385b2323717d40ff7f4a1d9afe4b11036
jaimil <jaimilkothari2003@gmail.com>
2025-02-24 18:02:53 +0530

    react folder added currency-converter

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -53,8 +53,8 @@ function App() {
           <button 
           onClick={copyPasswordToClipboard}
           className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer'>copy</button>
-
         </div>
+        
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
             <input
