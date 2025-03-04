727972f53625eb4f10d5da0f5fbd987da11449c4
dev_abhiraj <abhirajsinh2244@outlook.com>
2025-03-04 15:44:44 +0530

    restaurant

---
+++ restaurant/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Product from "./components/ProductCard";
+import Footer from "./components/Footer";  
+
+function App(){
+return (
+<>
+<Header/>
+<Body/>
+<Product/>  
+<Footer/>
+</>
+  
+);
+}
+export default App;
---
+++ restaurant/src/components/ProductCard.jsx
@@ -0,0 +1,10 @@
+function Product(props) {
+    return(
+<>
+<h1>{props.title}</h1>
+<h1>{props.price}</h1>
+<h1>{props.currency}</h1>
+</>
+    );
+}
+export default Product;
