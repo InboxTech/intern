837fb77eb0cd05917ef019fbf7b885c64bd08e30
aadil <aadil.work01@gmail.com>
2025-03-05 16:51:32 +0530

    add skyscanner

---
+++ React/skyscanner-clone/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FlightList from './components/flights';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React and Python Backend</h1>
+      <FlightList />
+    </div>
+  );
+};
+
+export default App;
